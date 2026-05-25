5e8ac6e13aa8370a0f08f44b2509b14766c08c01
irscavite <irscavite@gmail.com>
2026-05-25 15:55:16 +0800

    Delete constants.js

--- constants.js
+++
@@ -1,9 +0,0 @@
-export const userAccessMap = {
-    "easternirscavite@gmail.com": "ADMIN",
-    "maersk@mnr.com": "MAERSK",
-    "one@mnr.com": "ONE",
-    "cma@mnr.com": "CMA"
-};
-
-export const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-export const years = ["24", "25", "26"];
